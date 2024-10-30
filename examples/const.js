ad2abdae800069895afc886dc532dacac8975a0c
Erkebulan <erkebulanermekbaj5@gmail.com>
2024-10-30 12:42:49 +0600

    ozgertim

---
+++ examples/const.js
@@ -0,0 +1,44 @@
+var name = "Aty";
+
+let sur = "Familia";
+
+surname = "ABC";
+
+const id = 909090;
+
+id = 6598615341;
+
+console,log(id);
+
+
+let season = 'Fall';
+
+console.log(season === 'Fall');
+
+let name = "Erkebulan"
+
+let day = 30;
+
+let grades = [3, 4, 5, 8, 10]
+
+let name = "Askhat";
+let surname = "Allabergen";
+
+let age = 99;
+
+let weight = 70.45; 
+
+let streetname = "Maulenov, 92";
+
+let children = ["Asset", "Asqar", "Marat"];
+
+let car = {
+    name: "merc",
+    color: "black",
+    year: 2019
+}
+
+console.log("Onyn aty", name);
+console.log("Onyn tegi", surname);
+console.log("Neahede", age);
+console.log("Balalary", children);
