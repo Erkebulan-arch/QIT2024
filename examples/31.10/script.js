0fdb39464b806fc8c377a2d48305285f28a8e53f
Erkebulan <erkebulanermekbaj5@gmail.com>
2024-10-31 12:10:41 +0600

    tast1

---
+++ examples/31.10/script.js
@@ -0,0 +1,61 @@
+//let x = 4;
+
+//let y = 10;
+
+//let result = x+y;
+
+//console.log(result);
+
+//var x = 45;
+//var y = 5;
+
+//let result = x*y; 
+
+//console.log(result)
+
+//var c = 20;
+//var b = 2;
+
+//console.log(20 / 2); 
+
+//var a = 11; 
+//if (a % 2 == 0 ) {
+    //console.log(a, "sany jup san");
+//}
+
+//var num = 100;
+//num++; 
+//num--;
+//console.log(num);
+
+//var a = 100;
+//a += 5;
+//console.log(a);
+
+//var a = 100;
+//a - = 5; a = a - 5;
+//a / = 5; a = a / 5;
+//a * = 5; a = a * 5;
+//a % = 3; a = a % 3; a = 1;
+//console.log(a);
+
+// var a = 6;
+// var b = 3;
+// var c = 4;
+// var d = 2;
+// var e = 15;
+
+// var result = (a + b)*c-b/e;
+
+// console.log(result % 2);
+
+// console.log(a > b);
+
+var a = 4;
+var b = 6;
+var c = 4;
+var z = 5;
+
+var result = ((a * b) / c) / (z - 3);
+
+console.log(result % 2);
